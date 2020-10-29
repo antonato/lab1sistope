76b9ff0c30b44fdedfa9018ae4fc2caf0d437e12
Anto <antonia.arriagada@usach.cl>
2020-10-28 23:28:18 -0300

    Comparador: Se agrega archivo
    Se agrega archivo que compara una cadena en una linea de texto

---
+++ src/comparator.c
@@ -0,0 +1,63 @@
+/*
+Laboratorio 1, Sistemas Operativos
+Creadores: Antonina Arriagada, Francisco Núñez
+Fecha de creación: 28 de Octubre, 2020
+Última actualización: 28 de Octubre, 2020
+*/
+
+//Declaración de librerías
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+
+//Declaración de funciones y variables
+
+/*-------------------LÓGICA--------------------*/
+
+short int comparador(char *chain, char *line){
+
+	int lenC = strlen(chain);
+	int lenL = strlen(line);
+	// printf("lenC: %d\n", lenC);
+	// printf("lenL: %d\n", lenL);
+
+
+	for (int i = 0; i < lenL-(lenC-1); ++	i)
+	{
+		// printf("%c", line[i]);
+		if (chain[0]==line[i])
+		{
+			// printf("Encontre una similitud con %c \n", line[i]);
+			int j;
+			for (j = 1; j < lenC; j++)
+			{
+				if (chain[j]==line[i+1])
+				{
+					// printf("Encontre una similitud interna con %c \n", line[i+1]);
+					i++;
+				}
+				else{
+					i = i+1;
+					break;
+				}
+			}
+			if (j==lenC)
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	char test[30] = "AGAAAGGCATAAATATATG"; 
+	char chain[30] = "AGAAAGGCATAAATATATG"; 
+	short int respuesta = comparador(chain,test);
+	printf("Respuesta Comparador: %d", respuesta);
+}
