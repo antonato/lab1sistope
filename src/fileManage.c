e7d604024e0a04a99b0b3c51b760301d259cae27
Anto <antonia.arriagada@usach.cl>
2020-11-07 01:31:19 -0300

    Se agrega Makefile
    genera archivo final

--- src/fileManage.c
+++ src/fileManage.c
@@ -81,3 +81,49 @@ int writeFile(int row, char* nameFile, char ** lines, int len){
 
     return 0;
 }
+
+int makefile(int nProcess, char* chain){
+    FILE *final, *rest;
+    char c;
+   	
+    char file1[30]="rc_";
+   	char guion[] = "_";
+   	char extension[] = ".txt"; 
+    strcat(file1,chain);
+    strcat(file1,extension);
+    
+
+    printf("Nombre 1: %s\n", file1);
+
+    for (int i = 1; i <= nProcess; i++)
+    {
+        char file2[30]="rp_";
+        strcat(file2,chain);
+        char idst[3]; 
+	    sprintf(idst, "%d", i);
+        strcat(file2,guion);
+        strcat(file2,idst);
+        strcat(file2,extension);
+
+        rest=fopen(file2, "r+");
+        final=fopen(file1, "a");
+        printf("Nombre parcial: %s\n", file2);
+
+        while(!feof(rest))
+        {
+            fscanf(rest, "%c", &c);
+            fprintf(final, "%c", c);
+        }
+        fclose(rest);
+        fclose(final);
+    }
+        
+    return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    makefile(3, "AGGAA");
+    /* code */
+    return 0;
+}
